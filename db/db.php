66278451269c958c9f91093fbb14e3e9a83f9600
lionX1 <bricancecreative@gmail.com>
2025-04-27 14:33:15 +0100

    squelette du projet

---
+++ db/db.php
@@ -0,0 +1,9 @@
+<?php
+
+try {
+    $pdo = new PDO('mysql:host=localhost;dbname=correction_espace_membres;charset=UTF8', 'root', '');
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+    // $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $exception) {
+    exit("Oups, un problème s'est produit !");
+}
