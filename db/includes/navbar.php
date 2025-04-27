a12352660f72e2cefa5e4d2e3a080aa54279aefb
lionX1 <bricancecreative@gmail.com>
2025-04-27 14:33:15 +0100

    squelette du projet

---
+++ db/includes/navbar.php
@@ -0,0 +1,41 @@
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark" aria-label="Third navbar example">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Tableau de Bord</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03"
+            aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExample03">
+            <ul class="navbar-nav me-auto mb-2 mb-sm-0">
+                <!-- <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li> -->
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="dropdown03" data-bs-toggle="dropdown"
+                        aria-expanded="false">Gestion des utilisateurs</a>
+                    <ul class="dropdown-menu" aria-labelledby="dropdown03">
+                        <li><a class="dropdown-item text-secondary" href="../users/index.php">Liste des utilisateurs</a>
+                        </li>
+                        <li><a class="dropdown-item text-primary" href="../users/create.php">Ajouter un utilisateur</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="dropdown03" data-bs-toggle="dropdown"
+                        aria-expanded="false">Bienvenue
+                        <?php if (isset($_SESSION["name"])) { echo $_SESSION["name"]; } ?>
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="dropdown03">
+                        <li><a class="dropdown-item text-success" href="#">Accueil</a></li>
+                        <li><a class="dropdown-item text-primary" href="../users/profile.php">Profil</a></li>
+                        <li><a class="dropdown-item text-danger" href="../logout.php">Déconnexion</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <form>
+                <input class="form-control" type="text" placeholder="Search" aria-label="Search">
+            </form>
+        </div>
+    </div>
+</nav>
